e574beb440cee037b65f9d9dccf98290849905aa
Karimi <mostafa19.karimi91@gmail.com>
2022-08-06 20:17:50 +0430

    First Commit

---
+++ src/main/java/com/DockerChallenges/DockerChallenges.java
@@ -0,0 +1,13 @@
+package com.DockerChallenges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"com.DockerChallenges.controller"})
+public class DockerChallenges {
+    public static void main(String[] args) {
+        SpringApplication.run(DockerChallenges.class, args);
+    }
+}
