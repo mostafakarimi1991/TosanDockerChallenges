f223ffa722c6a7c8bedd73ce755e0fc71932ae72
Karimi <mostafa19.karimi91@gmail.com>
2022-08-06 20:19:36 +0430

    Add Rest Controllers

---
+++ src/main/java/com/DockerChallenges/controller/HelloController.java
@@ -0,0 +1,36 @@
+package com.DockerChallenges.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+
+@RestController
+@RequestMapping("/")
+@Slf4j
+public class HelloController {
+
+    @GetMapping("helloworld")
+    public ResponseEntity<String> helloStranger() {
+        log.info(OffsetDateTime.now().truncatedTo(ChronoUnit.SECONDS).toString());
+        String message = "Hello Stranger";
+        return new ResponseEntity<String>(message, HttpStatus.OK);
+    }
+
+    @PostMapping("helloworld")
+    public ResponseEntity<String> helloWithName(@RequestParam String name) {
+        log.info(OffsetDateTime.now().truncatedTo(ChronoUnit.SECONDS).toString());
+        String message = "Hello " + name;
+        return new ResponseEntity<String>(message, HttpStatus.OK);
+    }
+
+    @GetMapping("author")
+    public ResponseEntity<String> authorName() {
+        log.info(OffsetDateTime.now().truncatedTo(ChronoUnit.SECONDS).toString());
+        String message = "Mostafa Karimi";
+        return new ResponseEntity<String>(message, HttpStatus.OK);
+    }
+}
