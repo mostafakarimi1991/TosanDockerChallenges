56fd7597c63c867abb9be5dc5b40bd46e495539e
Karimi <mostafa19.karimi91@gmail.com>
2022-08-07 00:10:21 +0430

    Add unit tests

---
+++ src/test/java/unittest/HelloContrller/HelloController_helloWithName_UTest.java
@@ -0,0 +1,26 @@
+package unittest.HelloContrller;
+
+import com.DockerChallenges.controller.HelloController;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class HelloController_helloWithName_UTest {
+
+    @InjectMocks
+    HelloController helloController;
+
+    @BeforeEach
+    public void beforeAll(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void mustSayHelloToSpecialName(){
+        Assertions.assertEquals(new ResponseEntity<>("Hello Mostafa", HttpStatus.OK),helloController.helloWithName("Mostafa"));
+    }
+}
